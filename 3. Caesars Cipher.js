ecc3c2aa2eed98979510e9dc8de0f53728e98b21
Aman kumar sharma <kumaraman.rose@gmail.com>
2021-03-21 22:30:59 +0530

    Update 3. Caesars Cipher.js

--- 3. Caesars Cipher.js
+++ 3. Caesars Cipher.js
@@ -6,9 +6,6 @@ function rot13(str) {
   str = str.split("");
   for (var x in str) {
     if (regEx.test(str[x])) {
-      // A more general approach
-      // possible because of modular arithmetic
-      // and cyclic nature of rot13 transform
       rotCharArray.push(((str[x].charCodeAt() - 65 + 13) % 26) + 65);
     } else {
       rotCharArray.push(str[x].charCodeAt());
