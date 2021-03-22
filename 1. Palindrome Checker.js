270d873883060eb6e732a462099807dfeeb30f46
Aman kumar sharma <kumaraman.rose@gmail.com>
2021-03-22 11:43:41 +0530

    Update 1. Palindrome Checker.js

--- 1. Palindrome Checker.js
+++ 1. Palindrome Checker.js
@@ -1,12 +1,9 @@
 
 function palindrome(str) {
-  // Good luck!
   return str.replace(/[\W_]/g, '').toLowerCase() ===
          str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
          
   return true;
 }
-
-
 
 palindrome("eye");
